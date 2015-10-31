4d1f549869e2aed24253f2df662f1bddcf13b739
roberto <roberto@golgi.local>
2015-10-31 07:27:57 +0100

    add template base.php file
    This file has to be changed to contain user/password for DB (if a DB
    other than interactDB were used)

---
+++ php/base.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+$dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
+$dbuser = "YourDBUser_ProbablyROOT"; // the username that you created, or were given, to access your database
+$dbpass = "YourDBPassword"; // the password that you created, or were given, to access your database
+$dblogin = "Users";	// database containing the users list
+?>
