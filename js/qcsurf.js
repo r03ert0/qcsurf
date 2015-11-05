2830b39e6d65d37372f0c2caed4892b40d329b32
roberto <roberto@ramses.ghfc.pasteur.fr>
2015-11-05 13:24:16 +0100

    fix FS2ctm bash script

--- js/qcsurf.js
+++ js/qcsurf.js
@@ -373,3 +373,29 @@ function updateUser() {
  	done|tee $filename.txt
  done
 */
+
+/*
+ Now, this one it puts all values into a JSON file with all structures and measurements:
+ 
+ Killer one-liner (to execute inside the FS directory):
+ vals=( NumVert SurfArea GrayVol ThickAvg ThickStd MeanCurv GausCurv FoldInd CurvInd );echo -n "{";for ((k=0;k<9;k++)); do filename=${vals[k]};j=$((k+2));for ((i=54;i<88;i++)); do find . -name lh.aparc.stats|while read f; do awk 'NR=='$i'{print $1,$'$j'}' $f;done|awk 'BEING{s=0;ss=0;n=0}{n=n+1;split($0,arr," ");name=arr[1];s+=arr[2];ss+=arr[2]*arr[2]}END{print "'${vals[k]}'",name,s/n,sqrt(ss/n-s*s/n/n)}'; done;done|awk '{meas=$1;reg=$2;mean=$3;sd=$4;if(arr[reg])arr[reg]=arr[reg]",";arr[reg]=arr[reg]meas":{m:"mean",s:"sd"}"}END{n=0;for(i in arr)n++;j=0;for(i in arr){printf i":{"arr[i]"}";j++;if(j<n)print","}}'|sed -E 's/([a-zA-Z]+)/"\1"/g';echo "}"
+ 
+ Human-readable:
+ vals=( NumVert SurfArea GrayVol ThickAvg ThickStd MeanCurv GausCurv FoldInd CurvInd );
+ echo -n "{";
+ 
+ for ((k=0;k<9;k++)); do
+	 filename=${vals[k]};j=$((k+2));
+	 for ((i=54;i<88;i++)); do
+		find . -name lh.aparc.stats
+		|while read f; do
+			awk 'NR=='$i'{print $1,$'$j'}' $f;
+		done
+		|awk 'BEING{s=0;ss=0;n=0}{n=n+1;split($0,arr," ");name=arr[1];s+=arr[2];ss+=arr[2]*arr[2]}END{print "'${vals[k]}'",name,s/n,sqrt(ss/n-s*s/n/n)}';
+	 done;
+ done
+ |awk '{meas=$1;reg=$2;mean=$3;sd=$4;if(arr[reg])arr[reg]=arr[reg]",";arr[reg]=arr[reg]meas":{m:"mean",s:"sd"}"}END{n=0;for(i in arr)n++;j=0;for(i in arr){printf i":{"arr[i]"}";j++;if(j<n)print","}}'
+ |sed -E 's/([a-zA-Z]+)/"\1"/g';
+ 
+ echo "}"
+*/
--- resources/freesurfer_to_ctm.sh
+++ resources/freesurfer_to_ctm.sh
@@ -1,1 +1,5 @@
+mg=/Users/roberto/Applications/brainbits/m.meshgeometry/meshgeometry_mac
+td=/Applications/_Graph/vcglib/apps/tridecimator/tridecimator
+ml=/Applications/_Graph/meshlab-133.app/Contents/MacOS/meshlabserver
+
 find . -name *.ctm|while read f;do n=${f%.*};$ml -i $n.ctm -o $n.ply;rm $n.ctm;$td $n.ply $n.dec.ply 100000;rm $n.ply;$ml -i $n.dec.ply -o $n.ctm;rm $n.dec.ply;done
